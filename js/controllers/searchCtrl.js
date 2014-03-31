6f2e11c6c2a397fb760beced1f9a23f9b48d09dc
jon madison <jon@tfftech.com>
2014-03-31 14:29:09 -0700

    added addToBoard

--- js/controllers/searchCtrl.js
+++ js/controllers/searchCtrl.js
@@ -3,19 +3,25 @@ define(['angular', 'jquery'], function(angular){
     'use strict';
 
     instagram.popularMedia().success(function(data) {
-      $scope.currentMedia = data.data;
+      $scope.popularMedia = data.data;
     });
 
     $scope.$on('search', function(e, args){
       $log.info("Search for hashtag " + args.term);
 
       if (args.type == 'hashtag'){
-		instagram.hashtagMedia(args.term).success(function(data) {			
-			$scope.currentMedia = data.data;
-		})
-	  }
+        $scope.$apply(function() {
+          instagram.hashtagMedia(args.term).success(function(data) {			
+            $scope.searchResults = data.data;
+          })
+        });
+      }
     });	
 	
+    $scope.addBoard = function() {
+      console.log("Hovered")
+    };
+   
   };
 
   SearchCtrl.$inject = ['$scope', '$location', '$log', 'instagram'];
