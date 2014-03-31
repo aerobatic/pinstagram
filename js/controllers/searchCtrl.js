b62358cc2a828b4b384005cd09346b34fa322b69
David Von Lehman <david.vonlehman@gmail.com>
2014-03-31 13:38:04 -0700

    Layout search bar

--- js/controllers/searchCtrl.js
+++ js/controllers/searchCtrl.js
@@ -1,12 +1,21 @@
 
-define(['angular', 'jquery', 'jquery-isotope'], function(angular){
-  function SearchCtrl($scope, $location, instagram) {
+define(['angular', 'jquery'], function(angular){
+  function SearchCtrl($scope, $location, $log, instagram) {
     'use strict';
+
+    $scope.$on('search', function(e, args){
+      $log.info("Search for hashtag " + args.term);
+      // TODO: Execute search
+      // $scope.$apply(function(){
+      //   $scope.staticText = args;
+      // });
+    });
+
     instagram.popularMedia().success(function(data) {
       $scope.popularMedia = data.data;
     });
   };
 
-  SearchCtrl.$inject = ['$scope', '$location', 'instagram'];
+  SearchCtrl.$inject = ['$scope', '$location', '$log', 'instagram'];
   return SearchCtrl;
 });
