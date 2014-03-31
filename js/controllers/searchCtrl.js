8047039cb34e519aaed299200d81a1d4c1933d53
thomas-b-jackson <thomas.b.jackson@gmail.com>
2014-03-31 14:17:31 -0700

    update hashtag control

--- js/controllers/searchCtrl.js
+++ js/controllers/searchCtrl.js
@@ -3,27 +3,19 @@ define(['angular', 'jquery'], function(angular){
     'use strict';
 
     instagram.popularMedia().success(function(data) {
-      $scope.popularMedia = data.data;
+      $scope.currentMedia = data.data;
     });
 
     $scope.$on('search', function(e, args){
       $log.info("Search for hashtag " + args.term);
 
       if (args.type == 'hashtag'){
-		  $scope.$apply(function() {
-			instagram.hashtagMedia(args.term).success(function(data) {			
-				$scope.searchResults = data.data;
-			})
-        });
+		instagram.hashtagMedia(args.term).success(function(data) {			
+			$scope.currentMedia = data.data;
+		})
 	  }
     });	
 	
-    // var container = $('#container');
-    // container.isotope({
-    //   // options
-    //   itemSelector: '.item',
-    //   layoutMode: 'fitRows'
-    // });
   };
 
   SearchCtrl.$inject = ['$scope', '$location', '$log', 'instagram'];
