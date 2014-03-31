56e5184cd73deac1c690e3d7b680874182685b89
David Von Lehman <david.vonlehman@gmail.com>
2014-03-31 14:47:35 -0700

    Css treatment of layout

--- js/controllers/searchCtrl.js
+++ js/controllers/searchCtrl.js
@@ -1,31 +1,31 @@
-define(['angular', 'jquery'], function(angular){
-  function SearchCtrl($scope, $location, $log, instagram) {
-    'use strict';
+define(['angular', 'jquery'], function(angular) {
+	function SearchCtrl($scope, $location, $log, instagram) {
+		'use strict';
 
-    instagram.popularMedia().success(function(data) {
-      $scope.popularMedia = data.data;
-    });
+		instagram.popularMedia().success(function(data) {
+			$scope.currentMedia = data.data;
+		});
 
-    $scope.$on('search', function(e, args){
-      $log.info("Search for hashtag " + args.term);
+		$scope.$on('search', function(e, args) {
+			$log.info("Search for hashtag " + args.term);
 
-      if (args.type == 'hashtag'){
-		  $scope.$apply(function() {
-			instagram.hashtagMedia(args.term).success(function(data) {			
-				$scope.searchResults = data.data;
-			})
-        });
-	  }
-    });	
-	
-    // var container = $('#container');
-    // container.isotope({
-    //   // options
-    //   itemSelector: '.item',
-    //   layoutMode: 'fitRows'
-    // });
-  };
+			if (args.type == 'hashtag') {
+				// $scope.$apply(function() {
+				instagram.hashtagMedia(args.term).success(function(data) {
+					$log.debug("Search for hashtag complete");
+					$scope.currentMedia = data.data;
+				});
+			}
+		});
 
-  SearchCtrl.$inject = ['$scope', '$location', '$log', 'instagram'];
-  return SearchCtrl;
+		// var container = $('#container');
+		// container.isotope({
+		//   // options
+		//   itemSelector: '.item',
+		//   layoutMode: 'fitRows'
+		// });
+	};
+
+	SearchCtrl.$inject = ['$scope', '$location', '$log', 'instagram'];
+	return SearchCtrl;
 });
